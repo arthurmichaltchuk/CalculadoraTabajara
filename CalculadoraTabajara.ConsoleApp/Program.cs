c566ca00e0ba046542ae235bbaee8df3224cfc9f
Luisa Farias <luisafariasdemacedo@gmail.com>
2021-07-30 14:49:04 -0300

    Refatoração do menu

--- CalculadoraTabajara.ConsoleApp/Program.cs
+++ CalculadoraTabajara.ConsoleApp/Program.cs
@@ -12,14 +12,16 @@ namespace CalculadoraTabajara.ConsoleApp
         {
             #region Input
 
+            Console.WriteLine("Digite a operação desejada: \n 1- somar\n 2- subtrair\n 3-dividir \n 4-multiplicar");
+            string operacao = Console.ReadLine(); 
+
             Console.WriteLine("Digite o primeiro número: ");
             int primeiroNumero = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Digite o segundo número: ");
             int segundoNumero = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite a operação desejada: \n 1- somar\n 2- subtrair\n 3-multiplicar \n 4-dividir");
-            string operacao = Console.ReadLine();
+            
 
             #endregion
 
